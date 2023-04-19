34d625a288a91808a2c9571943abc99263bcdbc6
Arthur Pedroti <arthurpedroti@gmail.com>
2023-04-19 17:34:49 -0300

    routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import fastify from 'fastify'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+import fastifyCookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.register(fastifyCookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions'
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT
   })
   .then(() => {
     console.log('HTTP Server Running!')
